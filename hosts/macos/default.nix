e35ee3880bb71aab9028896cf974553c20ae5817
Alexandr Timchenko <atimchenko92@gmail.com>
2026-01-31 22:24:26 +0100

    chore(macos): remove repeated packages

--- hosts/macos/default.nix
+++ hosts/macos/default.nix
@@ -33,10 +33,8 @@
 
     casks = [
       "balenaetcher"
-      "emacs-app"
       "gimp"
       "google-chrome" # Added (works best via Cask on macOS)
-      "keepassxc"
       "libreoffice"
       "openmtp"
       "supercollider"
