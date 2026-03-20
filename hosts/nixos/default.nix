7b426e6d1ef3985705ab56f44426c8b0e4465392
Alexandr Timchenko <atimchenko92@gmail.com>
2026-03-20 09:40:40 +0100

    feat(pipewire): increase buffer to eliminate crackling sounds

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -117,6 +117,14 @@ in
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    extraConfig.pipewire."92-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 1024;
+        "default.clock.min-quantum" = 512;
+        "default.clock.max-quantum" = 2048;
+      };
+    };
   };
 
   # DE
