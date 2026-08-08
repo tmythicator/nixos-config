ea3292c23b6238f3f5b667fe1f7351f927d30895
Alexandr Timchenko <atimchenko92@gmail.com>
2026-08-08 12:28:12 +0200

    feat(xdg): add mime type links to apps

--- home/nixos/default.nix
+++ home/nixos/default.nix
@@ -46,4 +46,21 @@ in
       upgrade = "nix flake update --flake ${flakeDir} && update";
     };
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = [ "firefox.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/about" = [ "firefox.desktop" ];
+      "x-scheme-handler/unknown" = [ "firefox.desktop" ];
+      "application/pdf" = [ "firefox.desktop" ];
+      "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+      "image/png" = [ "org.gnome.eog.desktop" ];
+      "image/jpeg" = [ "org.gnome.eog.desktop" ];
+      "video/mp4" = [ "vlc.desktop" ];
+      "video/mkv" = [ "vlc.desktop" ];
+    };
+  };
 }
